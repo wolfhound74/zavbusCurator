49ccf976644363dca456a5ded0b131d481f2df56
wolfhound <ripper_roo@mail.ru>
2018-01-25 15:55:20 +0500

    first commmit

---
+++ zavbusCurator/Controllers/BusTripTableViewController.swift
@@ -0,0 +1,133 @@
+import UIKit
+
+class BusTripTableViewController: UITableViewController {
+
+    var trips = [BusTrip]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.getDataFromServer()
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trips.count
+    }
+
+    @IBAction func pullTripButton(_ sender: Any) {
+        let alert = UIAlertController(
+                title: "Список выездов",
+                message: "Забрать данные с сервера?",
+                preferredStyle: .alert)
+
+        alert.addTextField { (textField: UITextField!) -> Void in
+            textField.placeholder = "username"
+        }
+        alert.addTextField { (textField: UITextField!) -> Void in
+            textField.placeholder = "пароль"
+        }
+
+        let defaultAction = UIAlertAction(title: "Скачать", style: .default) { (alert) in
+            self.getDataFromServer()
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .default) { (alert) in
+            print("cancel")
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+
+        cell.textLabel?.text = trips[indexPath.row].name
+        cell.detailTextLabel?.text = trips[indexPath.row].dates
+
+        return cell
+    }
+
+    private func getDataFromServer() {
+//        let url = URL(string: "http://zavbus.team/api/curatorData")
+        let url = URL(string: "http://localhost:8090/api/curatorData")
+        do {
+            let data = try Data(contentsOf: url!)
+            do {
+                let dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSDictionary
+                let loadTrips = dict.object(forKey: "trips") as! Array<NSDictionary>
+                for var trip in loadTrips {
+                    trips.append(BusTrip(dictionary: trip))
+                }
+            } catch {
+            }
+        } catch {
+        }
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tripRecords" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let records = trips[indexPath.row].records
+                let controller = segue.destination as! TripRecordTableViewController
+                controller.records = records
+                controller.trip = trips[(tableView.indexPathForSelectedRow?.row)!]
+
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+
+}
